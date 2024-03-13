0a9c2c3df7fe628a62b7b8236f560a2731a6ea4b
Uchenna <76819290+Uche-Uche@users.noreply.github.com>
2024-03-13 10:25:36 +0100

    Update my_list_is_happy.py

--- my_list_is_happy.py
+++ my_list_is_happy.py
@@ -1,9 +1,10 @@
+#create a function
 def my_List():
     #create an empty list called my_list
     my_list=[]
     #append elements to my_list
     elements=[10, 20, 30, 40]
-    #loop through the each element and append to the list
+    #loop through elements and append each one to the list
     for each_number in elements:
         my_list.append(each_number)
     #insert the value 15 to the second position in the list
@@ -17,5 +18,8 @@ def my_List():
     #print the index of the value 30
     index=my_list.index(30)
     print(f"the index of 30 is {index}")
+    #print my_list
     print(f"my_list is= {my_list}")
+
+#call the function
 my_List()
