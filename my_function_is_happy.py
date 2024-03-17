97e9a567df384d777e06c54491ab7ef7324ae844
Uchenna <76819290+Uche-Uche@users.noreply.github.com>
2024-03-17 19:55:47 +0100

    Add files via upload

---
+++ my_function_is_happy.py
@@ -0,0 +1,28 @@
+#function to add two numbers
+def addTwo(first_number, second_number):
+    sum=first_number+second_number
+    return sum
+
+#function to subtract two numbers
+def subtractTwo(first_number, second_number):
+    difference=first_number-second_number
+    return difference
+
+#function to multiply two numbers
+def multiplyTwo(first_number, second_number):
+    product=first_number*second_number
+    return product
+
+#function to divide two numbers
+def divideTwo(first_number, second_number):
+    quotient=first_number*second_number
+    return quotient
+
+#function to return the length of a string
+def stringLength(string):
+    return len(string)
+
+#function to return the first element of a given string
+def getFirstElement(given_list):
+    return given_list[0]
+    
